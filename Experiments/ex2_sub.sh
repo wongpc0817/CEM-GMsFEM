6da8304d79b7fa9a491e8f4827493012eca85cb9
Changqing YE <ye_changqing@outlook.com>
2021-10-16 00:50:00 +0800

    1016-2021

--- Experiments/ex2_sub.sh
+++
@@ -1,6 +0,0 @@
-#PBS -S /bin/bash
-#PBS -N EX3
-
-cd ~/CEM-GMsFEM/
-
-python ~/CEM-GMsFEM/Experiments/ex2.py 3
--- Experiments/ex4_sub.sh
+++
@@ -1,6 +0,0 @@
-#PBS -S /bin/bash
-#PBS -N EX4
-
-cd ~/CEM-GMsFEM/
-
-python ~/CEM-GMsFEM/Experiments/ex4.py 3
