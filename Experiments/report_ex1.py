599071a97d730a2620ec74f03da5d3cd89e41e68
Buji Wong <bujiwong0817@gmail.com>
2023-08-22 14:13:46 +0800

    edit report.py

--- Experiments/report_ex1.py
+++ Experiments/report_ex1.py
@@ -2,9 +2,9 @@ import numpy as np
 line_end = ' \\\\ \n'
 table = ''
 table += 'Model problem 1\n'
-lambdas = [2.298640,2.299099,2.299145]
-eg_norm = [68.392766,216.318225,684.071373]
-l2_norm = [0.007564,0.007567,0.007567]
+lambdas = [3.319007,3.319007,3.319007]
+eg_norm = [66.667711,211.044773,670.470387]
+l2_norm = [0.007169,0.007174,0.007227]
 ratios=[10**i for i in [4,5,6]]
 numEig = len(eg_norm)
 table += '-' * 80 + '\n'
@@ -12,13 +12,13 @@ table += '$\kappa_1/\kappa_m$&'+'&'.join(['\\num{{{:.3e}}}'.format(v) for v in r
 table += '$\|u\|_a$ & ' + ' & '.join(['\\num{{{:.3e}}}'.format(v) for v in eg_norm]) + line_end
 table += '$\|u\|_{L^2(\Omega)}$ & ' + ' & '.join(['\\num{{{:.3e}}}'.format(v) for v in l2_norm]) + line_end
 table += '$\Lambda^\prime$ &'+'&'.join(['\\num{{{:.3e}}}'.format(lambdas[i])for i in range(numEig)])+line_end
-err_eg = np.array([[0.829295,  0.023058,  0.000548,  0.000025],
-                   [2.621876,  0.072883,  0.001731,  0.000078],
-                   [0.000292,  0.000009,  0.000000,  0.000000]]).T.tolist()
+err_eg = np.array([[0.588712,  0.013972,  0.000488,  0.000020],
+                   [1.862620,  0.044102,  0.001544,  0.000064],
+                   [5.838741,  0.140616,  0.004883,  0.000204]]).T.tolist()
 
-err_l2 = np.array([[0.000292,  0.000009,  0.000000,  0.000000],
-                   [0.000292,  0.000009,  0.000000,  0.000000],
-                   [8.290914,  0.230465,  0.005474,  0.000245]]).T.tolist()
+err_l2 = np.array([[0.000188,  0.000005,  0.000000,  0.000000],
+                   [0.000189,  0.000005,  0.000000,  0.000000],
+                   [0.000186,  0.000005,  0.000000,  0.000000]]).T.tolist()
 
 table += '-' * 80 + '\n'
 table += '$\kappa_1/\kappa_m$&'+'&'.join(['\\num{{{:.3e}}}'.format(v) for v in ratios])+line_end
---
+++ Experiments/report_ex3.py
@@ -0,0 +1,58 @@
+import numpy as np
+line_end = ' \\\\ \n'
+table = ''
+table += 'Model problem 2\n'
+table += '-' * 80 + '\n'
+
+#ratio = 10^4, H=1/20, l_m=3, N_ov=1->4
+err_l2=np.array([[0.002695,0.002616,0.002616,0.000016],
+                [0.014924,0.005584,0.000044,0.000040],
+                [0.015028,0.011337,0.000088,0.000050]]).T.tolist()
+err_eg=np.array([[0.715295,0.713042,0.713039,0.001509],
+                [0.089634,0.051678,0.003955,0.003156],
+                [0.092893,0.073729,0.006870,0.003957]]).T.tolist()
+l2_norm=np.array([[1.862780,1.862775,1.862775,0.015749],
+                [0.015749,0.015749,0.015749,0.015749],
+                [0.015749,0.015749,0.015749,0.015749]]).T.tolist()
+eg_norm=np.array([[2.843904,2.843902,2.843902,0.220558],
+                [0.220558,0.220558,0.220558,0.220558],
+                [0.220558,0.220558,0.220558,0.220558]]).T.tolist()
+
+Hlist = [1/i for i in [20,40,80]]
+table += '$H$&' +'&'.join(['\\num{{{:1f}}}'.format(v) for v  in Hlist])+line_end
+for i in range(4):
+    table+=f'$E^{i}_a$&'+'&'.join(['\\num{{{:.3e}}}'.format(err_eg[i][j]/eg_norm[i][j]) for j in range(3)])+line_end
+for i in range(4):
+    table+=f'$E^{i}_L$&'+'&'.join(['\\num{{{:.3e}}}'.format(err_l2[i][j]/l2_norm[i][j]) for j in range(3)])+line_end
+
+table+='-'*80 +line_end
+#H=1/80, ratio=10^3-> , l_m=3
+err_l2=[0.018517,0.015028,0.0147110,0.014680]
+err_eg=[0.143817,0.092893,0.0849320,0.084072]
+l2_norm=[0.019419,0.015749,0.0154100,0.015377]
+eg_norm=[0.250827,0.220558,0.2167690,0.216378]
+
+ratios= [10**i for i in range(3,7)]
+table+= '$\kappa_1/\kappa_m$&'+'&'.join(['\\num{{{:.3e}}}'.format(v) for v in ratios])+line_end
+table+='$E^1_a$&'+'&'.join(['\\num{{{:.3e}}}'.format(err_eg[i]/eg_norm[i])for i in range(4)])+line_end
+table+='$\|u\|_{a(\Omega)}$&'+'&'.join(['\\num{{{:.3e}}}'.format(eg_norm[i])for i in range(4)])+line_end
+table+='$E^1_L$&'+'&'.join(['\\num{{{:.3e}}}'.format(err_l2[i]/l2_norm[i])for i in range(4)])+line_end
+table+='$\|u\|_{L^2(\Omega)}$&'+'&'.join(['\\num{{{:.3e}}}'.format(l2_norm[i])for i in range(4)])+line_end
+
+table+='-'*80+line_end
+# H=1/80, ratio=10^4, N_ov=3
+err_l2=[0.006061,0.000091,0.000088,0.000084]
+err_eg=[0.053911,0.006984,0.006870,0.006675]
+l2_norm=[0.015749,0.015749,0.015749,0.015749]
+eg_norm=[0.220558,0.220558,0.220558,0.220558]
+
+numEig =[1,2,3,4]
+table+= '$l_m$&'+'&'.join([f'{v}' for v in numEig])+line_end
+
+table+='$E^3_a$&'+'&'.join(['\\num{{{:.3e}}}'.format(err_eg[i]/eg_norm[i])for i in range(4)])+line_end
+table+='$\|u\|_{a(\Omega)}$&'+'&'.join(['\\num{{{:.3e}}}'.format(eg_norm[i])for i in range(4)])+line_end
+table+='$E^3_L$&'+'&'.join(['\\num{{{:.3e}}}'.format(err_l2[i]/l2_norm[i])for i in range(4)])+line_end
+table+='$\|u\|_{L^2(\Omega)}$&'+'&'.join(['\\num{{{:.3e}}}'.format(l2_norm[i])for i in range(4)])+line_end
+
+
+print(table)
